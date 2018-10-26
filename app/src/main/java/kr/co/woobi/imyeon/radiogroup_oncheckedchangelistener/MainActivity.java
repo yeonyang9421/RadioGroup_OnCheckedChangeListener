9ba51b025e757fe351481574d09c21d0caef7b80
yeonyang9421 <yeonyang21@gmail.com>
2018-10-26 17:02:13 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/woobi/imyeon/radiogroup_oncheckedchangelistener/MainActivity.java
@@ -0,0 +1,25 @@
+package kr.co.woobi.imyeon.radiogroup_oncheckedchangelistener;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                String text="ChechedId : " + checkedId;
+                Toast.makeText(getApplicationContext(),text,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
